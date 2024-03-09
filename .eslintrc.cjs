13c6cdc13c7379bb64fd5dd06fe2566da89168c3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-09 17:21:34 -0500

    Fix eslint config and ignore tailwind configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,8 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-strict-type-checked",
-    `plugin:@typescript-eslint/stylistic-type-checked`,
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
