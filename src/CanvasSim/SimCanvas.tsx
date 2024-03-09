50e6c6267c091bf142d5050955fdbeed170dfba0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-09 18:36:50 -0500

    Add useEffects for initialization and animation starting

--- src/CanvasSim/SimCanvas.tsx
+++ src/CanvasSim/SimCanvas.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
 const SimCanvas = () => {
-  return <canvas className="bg-black h-full w-full"></canvas>;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const contextRef = useRef<CanvasRenderingContext2D | null>(null);
+  const [canvasInitialized, setCanvasInitialized] = useState<boolean>(false);
+
+  // Initialize canvas by setting context ref
+  useEffect(() => {
+    if (canvasRef.current) {
+      contextRef.current = canvasRef.current.getContext("2d");
+      setCanvasInitialized(true);
+      console.log("Canvas intialized.");
+    }
+  }, []);
+
+  // Start the animation if canvas is initialized
+  useEffect(() => {
+    if (canvasInitialized) {
+      // Start animation loop with requestAnimationFrame
+      console.log("Animation started.");
+    }
+  }, [canvasInitialized]);
+
+  return <canvas className="bg-black h-full w-full" ref={canvasRef}></canvas>;
 };
 
 export default SimCanvas;
