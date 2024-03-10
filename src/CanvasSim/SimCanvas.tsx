ccedfa17397f03cb776a0957d512836ef8c26c74
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-09 19:01:39 -0500

    Create a particle on click at click location

--- src/CanvasSim/SimCanvas.tsx
+++ src/CanvasSim/SimCanvas.tsx
@@ -1,9 +1,36 @@
 import { useEffect, useRef, useState } from "react";
+import { ParticleInterface } from "./defs";
+import createParticle from "./createParticle";
 
 const SimCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
   const [canvasInitialized, setCanvasInitialized] = useState<boolean>(false);
+  const particles = useRef<ParticleInterface[]>([]);
+
+  // Handle clicks by creating a particle with random properties
+  const handleClick = (event: React.MouseEvent) => {
+    if (canvasRef.current) {
+      // Get mouse position on canvas
+      const rect = canvasRef.current.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+
+      // Create random particle with x and y
+      const newParticle = createParticle({
+        x,
+        y,
+        vx: 1,
+        vy: 1,
+        mass: 1,
+        radius: 2,
+        color: "yellow",
+      });
+
+      // Add it to particles array
+      particles.current.push(newParticle);
+    }
+  };
 
   // Initialize canvas by setting context ref
   useEffect(() => {
@@ -22,7 +49,13 @@ const SimCanvas = () => {
     }
   }, [canvasInitialized]);
 
-  return <canvas className="bg-black h-full w-full" ref={canvasRef}></canvas>;
+  return (
+    <canvas
+      onClick={handleClick}
+      className="bg-black h-full w-full"
+      ref={canvasRef}
+    ></canvas>
+  );
 };
 
 export default SimCanvas;
