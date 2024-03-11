1a63d5d27559c418dae1d34bd14ea9ba1bf1b322
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-10 23:14:14 -0400

    Remove move method

--- src/CanvasSim/createParticle.ts
+++ src/CanvasSim/createParticle.ts
@@ -25,14 +25,6 @@ const createParticle = ({
     mass,
     radius,
     color,
-    move: (force: { x: number; y: number }) => {
-      // Update velocity based on force
-      particle.vx += force.x / mass;
-      particle.vy += force.y / mass;
-      // Update position based on velocity
-      particle.x += particle.vx;
-      particle.y += particle.vy;
-    },
   };
 
   return particle;
