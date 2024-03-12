a9c0cf2abd1cc1de762f8e220992d438c08eeca8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-12 19:41:52 -0400

    Console log default return which should not happen

--- src/CanvasSim/createQuadtree.ts
+++ src/CanvasSim/createQuadtree.ts
@@ -60,29 +60,35 @@ const createQuadTree = (boundary: Rectangle, capacity: PositiveInteger) => {
   // Method for inserting into tree
   const insert = (point: ParticleInterface) => {
     // Return if the point is not within boundary
-    if (!rectContains(boundary, point)) return;
+    if (!rectContains(boundary, point)) return false;
 
     // Add point if there is room and the node is not divided, else subdivide recursively
     if (quadTree.points.length < capacity && !quadTree.divided) {
       quadTree.points.push(point);
+      return true;
     } else {
       if (!quadTree.divided) {
         subdivide();
         // Recursively add the divided nodes points to new child nodes
         quadTree.points.forEach((point) => {
-          quadTree.northwest?.insert(point);
-          quadTree.northeast?.insert(point);
-          quadTree.southeast?.insert(point);
-          quadTree.southwest?.insert(point);
-          // Clear points from parent node
-          quadTree.points = [];
+          if (quadTree.northwest?.insert(point)) return true;
+          if (quadTree.northeast?.insert(point)) return true;
+          if (quadTree.southeast?.insert(point)) return true;
+          if (quadTree.southwest?.insert(point)) return true;
         });
       }
-      quadTree.northwest?.insert(point);
-      quadTree.northeast?.insert(point);
-      quadTree.southeast?.insert(point);
-      quadTree.southwest?.insert(point);
+
+      // Clear points from parent node
+      quadTree.points = [];
+
+      // Add the point to the children nodes
+      if (quadTree.northwest?.insert(point)) return true;
+      if (quadTree.northeast?.insert(point)) return true;
+      if (quadTree.southeast?.insert(point)) return true;
+      if (quadTree.southwest?.insert(point)) return true;
     }
+    console.log("Default false return. Point not inserted. Duplicate points?");
+    return false;
   };
 
   // Create and return quadtree object
