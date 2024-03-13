4b9dcd7c25a09c479e76286777e0733b6746f958
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-13 15:24:13 -0400

    Add fps prop

--- src/CanvasSim/HUD.tsx
+++ src/CanvasSim/HUD.tsx
@@ -1,14 +1,16 @@
 const HeadsUpDisplay = ({
   mousePosX,
   mousePosY,
+  fps,
 }: {
   mousePosX: number | null;
   mousePosY: number | null;
+  fps: number | null;
 }) => {
   return (
     <div className="absolute top-0 text-neutral-50 h-full w-full pointer-events-none">
       <div className="p-5">
-        <p>FPS:</p>
+        <p>FPS: {fps}</p>
         <p>MouseX: {mousePosX}</p>
         <p>MouseY: {mousePosY}</p>
       </div>
