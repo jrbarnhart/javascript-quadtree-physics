d171aa7684b3cf4286c3a658a6c8335014382d07
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-13 14:47:09 -0400

    Style text with color and padding

--- src/CanvasSim/HUD.tsx
+++ src/CanvasSim/HUD.tsx
@@ -1,6 +1,6 @@
 const HeadsUpDisplay = () => {
   return (
-    <div className="absolute top-0">
+    <div className="absolute top-0 text-neutral-50 p-5">
       <p>FPS:</p>
       <p>MouseX:</p>
       <p>MouseY:</p>
