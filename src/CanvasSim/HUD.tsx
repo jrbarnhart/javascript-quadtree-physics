b298f6b3e262e408b7ec6e54f1838a47da38a770
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-13 14:44:22 -0400

    Add simple HUD component

---
+++ src/CanvasSim/HUD.tsx
@@ -0,0 +1,11 @@
+const HeadsUpDisplay = () => {
+  return (
+    <div className="absolute top-0">
+      <p>FPS:</p>
+      <p>MouseX:</p>
+      <p>MouseY:</p>
+    </div>
+  );
+};
+
+export default HeadsUpDisplay;
