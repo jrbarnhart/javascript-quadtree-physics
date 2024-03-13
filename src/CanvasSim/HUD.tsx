4fb037ac68393ac55c27bc01e2b8d66545d92508
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-13 18:43:55 -0400

    Add total particles to hud

--- src/CanvasSim/HUD.tsx
+++ src/CanvasSim/HUD.tsx
@@ -1,15 +1,18 @@
 const HeadsUpDisplay = ({
   mousePosX,
   mousePosY,
+  totalParticles,
 }: {
   mousePosX: number | null;
   mousePosY: number | null;
+  totalParticles: number | null;
 }) => {
   return (
     <div className="absolute top-0 grid items-end text-neutral-50 h-full w-full pointer-events-none">
       <div className="p-5">
         <p>MouseX: {mousePosX}</p>
         <p>MouseY: {mousePosY}</p>
+        <p>Particles: {totalParticles}</p>
       </div>
     </div>
   );
