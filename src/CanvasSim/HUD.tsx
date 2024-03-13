3d1f7d7b19d43dc0745f16688c1a3fd4a32f713a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-13 15:11:48 -0400

    Set mousex and y info from props

--- src/CanvasSim/HUD.tsx
+++ src/CanvasSim/HUD.tsx
@@ -1,9 +1,17 @@
-const HeadsUpDisplay = () => {
+const HeadsUpDisplay = ({
+  mousePosX,
+  mousePosY,
+}: {
+  mousePosX: number | null;
+  mousePosY: number | null;
+}) => {
   return (
-    <div className="absolute top-0 text-neutral-50 p-5">
-      <p>FPS:</p>
-      <p>MouseX:</p>
-      <p>MouseY:</p>
+    <div className="absolute top-0 text-neutral-50 h-full w-full pointer-events-none">
+      <div className="p-5">
+        <p>FPS:</p>
+        <p>MouseX: {mousePosX}</p>
+        <p>MouseY: {mousePosY}</p>
+      </div>
     </div>
   );
 };
