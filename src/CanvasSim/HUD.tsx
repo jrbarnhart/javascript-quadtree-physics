a05cc931a5c69bccf97c12cf73eaef77bbdd50d7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-20 02:54:12 -0400

    Set state on button click for drawing rects

--- src/CanvasSim/HUD.tsx
+++ src/CanvasSim/HUD.tsx
@@ -2,11 +2,18 @@ const HeadsUpDisplay = ({
   mousePosX,
   mousePosY,
   totalParticles,
+  setDrawQuadtree,
 }: {
   mousePosX: number | null;
   mousePosY: number | null;
   totalParticles: number | null;
+  setDrawQuadtree: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const handleShowClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setDrawQuadtree((previous) => !previous);
+  };
+
   return (
     <>
       <div className="absolute top-0 grid items-end text-neutral-50 h-full w-full pointer-events-none">
@@ -16,7 +23,10 @@ const HeadsUpDisplay = ({
           <p>Particles: {totalParticles}</p>
         </div>
       </div>
-      <button className="absolute bottom-0 right-0 text-neutral-950 p-2 m-5 bg-neutral-50 rounded-md active:bg-neutral-400">
+      <button
+        onClick={handleShowClick}
+        className="absolute bottom-0 right-0 text-neutral-950 p-2 m-5 bg-neutral-50 rounded-md active:bg-neutral-400"
+      >
         Show Quadtree
       </button>
     </>
