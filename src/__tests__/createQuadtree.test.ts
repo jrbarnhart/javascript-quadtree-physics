db88880ed3f8e0cb1e12cf5d316b54d69bc299ab
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-20 15:45:44 -0400

    Test subdivideNode for correct behavior

--- src/__tests__/createQuadtree.test.ts
+++ src/__tests__/createQuadtree.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, test } from "vitest";
-import { getChildForParticle } from "../CanvasSim/createQuadtree";
+import {
+  getChildForParticle,
+  subdivideNode,
+} from "../CanvasSim/createQuadtree";
 import {
   ParticleInterface,
   Quadtree,
@@ -91,5 +94,113 @@ describe("getChildForParticle", () => {
     expect(childNE).toStrictEqual(testChildNE);
     expect(childSE).toStrictEqual(testChildSE);
     expect(childSW).toStrictEqual(testChildSW);
+  });
+});
+
+describe("subdivideNode", () => {
+  test("Node with children will not be subdivided", () => {
+    // testRect is also used for child bounaries as they are irrelevant to test
+    const testRect: QuadtreeBoundary = {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+    };
+    // Mock quadtree and children
+    const testChildNW: Quadtree = {
+      particles: [],
+      children: [],
+      boundary: testRect,
+    };
+    const testChildNE: Quadtree = {
+      particles: [],
+      children: [],
+      boundary: testRect,
+    };
+    const testChildSE: Quadtree = {
+      particles: [],
+      children: [],
+      boundary: testRect,
+    };
+    const testChildSW: Quadtree = {
+      particles: [],
+      children: [],
+      boundary: testRect,
+    };
+    const testTree: Quadtree = {
+      particles: [],
+      children: [testChildNW, testChildNE, testChildSE, testChildSW],
+      boundary: testRect,
+    };
+
+    subdivideNode(testTree);
+
+    expect(testTree.children[0]).toStrictEqual(testChildNW);
+    expect(testTree.children[1]).toStrictEqual(testChildNE);
+    expect(testTree.children[2]).toStrictEqual(testChildSE);
+    expect(testTree.children[3]).toStrictEqual(testChildSW);
+  });
+
+  test("Node with no children will have four child trees added to it", () => {
+    const testRect: QuadtreeBoundary = {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+    };
+    const testTree: Quadtree = {
+      particles: [],
+      children: [],
+      boundary: testRect,
+    };
+
+    subdivideNode(testTree);
+
+    expect(testTree.children.length).toBe(4);
+    expect(testTree.children[0]).toHaveProperty("particles", []);
+    expect(testTree.children[1]).toHaveProperty("particles", []);
+    expect(testTree.children[2]).toHaveProperty("particles", []);
+    expect(testTree.children[3]).toHaveProperty("particles", []);
+  });
+
+  test("Node's new children have correct boundaries", () => {
+    const testRect: QuadtreeBoundary = {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 200,
+    };
+    const testTree: Quadtree = {
+      particles: [],
+      children: [],
+      boundary: testRect,
+    };
+
+    subdivideNode(testTree);
+
+    expect(testTree.children[0].boundary).toStrictEqual({
+      x: 0,
+      y: 0,
+      width: 50,
+      height: 100,
+    });
+    expect(testTree.children[1].boundary).toStrictEqual({
+      x: 50,
+      y: 0,
+      width: 50,
+      height: 100,
+    });
+    expect(testTree.children[2].boundary).toStrictEqual({
+      x: 50,
+      y: 100,
+      width: 50,
+      height: 100,
+    });
+    expect(testTree.children[3].boundary).toStrictEqual({
+      x: 0,
+      y: 100,
+      width: 50,
+      height: 100,
+    });
   });
 });
