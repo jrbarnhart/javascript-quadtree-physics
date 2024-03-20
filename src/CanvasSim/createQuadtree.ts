e1b1b5aa7a24f59bc1bca8a2f9d58a3afb3bfd6f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-19 20:12:39 -0400

    Change console logs

--- src/CanvasSim/createQuadtree.ts
+++ src/CanvasSim/createQuadtree.ts
@@ -114,7 +114,7 @@ const barnesHutCalculation = (
   quadTree: QuadTree
 ) => {
   queryNodePoints.forEach((pointA) => {
-    // If the quadtree node compared against point (starting with root) is external
+    // If the quadtree node is external and has points apply gravity b/w pointA and all of QT's points
     if (!quadTree.divided && quadTree.points.length > 0) {
       console.log("FOUND an external node with particles. UPDATING PARTICLES!");
       // Calculate gravity between point and points in edge node
@@ -143,7 +143,11 @@ const barnesHutCalculation = (
       quadTree.massCenterX !== null &&
       quadTree.massCenterY !== null
     ) {
-      console.log("FOUND internal node. Checking distance to its center.");
+      console.log(
+        "FOUND internal node. Checking distance to its center.",
+        quadTree.depth,
+        quadTree.massTotal
+      );
       const { distance, distSq, dx, dy } = calculateDistance(
         pointA.x,
         pointA.y,
@@ -153,7 +157,9 @@ const barnesHutCalculation = (
       // If s/d < theta approximate gravity using center of mass
       const s = (quadTree.boundary.width + quadTree.boundary.height) / 2;
       if (s / distance < THETA) {
-        console.log("Internal node cmass far enough to approximate.");
+        console.log(
+          "Internal node cmass far enough to approximate. UPDATING APPROXIMATE PARTICLES!"
+        );
         // Approximate gravity
         const gravForce = calculateAttraction(
           dx,
