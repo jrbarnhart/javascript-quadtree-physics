9ccc3b68cfa0582152e3cf532f73c6a11d8c50a8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-22 05:00:42 -0400

    Implement new quadtree and update drawRects

--- src/CanvasSim/animate.ts
+++ src/CanvasSim/animate.ts
@@ -1,6 +1,5 @@
-import { ParticleInterface, QuadTree } from "./defs";
-import createQuadTree from "./createQuadtreeOld";
-import createRectangle from "./createRectangle";
+import { ParticleInterface, Quadtree, QuadtreeBoundary } from "./defs";
+import createQuadtree from "./createQuadtree";
 
 const animate = ({
   particles,
@@ -16,54 +15,21 @@ const animate = ({
   drawQuadtree: boolean;
 }) => {
   // Create the quadtree from particles
-  const boundary = createRectangle(
-    canvasWidth / 2,
-    canvasHeight / 2,
-    canvasHeight,
-    canvasWidth
-  );
-  const quadTree = createQuadTree(boundary, 1);
+  const boundary: QuadtreeBoundary = {
+    x: 0,
+    y: 0,
+    height: canvasHeight,
+    width: canvasWidth,
+  };
+  const quadTree = createQuadtree({ boundary, particles });
 
-  /*   // Brute force gravity
-  const maxVelocity = 0.1;
-  // For each particle
-  particles.forEach((particle) => {
-    // Insert particle into quadtree
-    quadTree.insert(particle);
-
-    // Calc gravitational force
-    const gravity = calculateGravity(particle, particles);
-    // Move particle using force, clamping to min/max values
-    // Update velocity based on force
-    particle.vx = Math.max(
-      -maxVelocity,
-      Math.min(particle.vx + gravity.x / particle.mass, maxVelocity)
-    );
-    particle.vy = Math.max(
-      -maxVelocity,
-      Math.min(particle.vy + gravity.y / particle.mass, maxVelocity)
-    );
-    // Update position based on velocity
-    particle.x += particle.vx;
-    particle.y += particle.vy;
-    // Reverse velocity on boundary collision
-    if (particle.x < 0 || particle.x > canvasWidth) {
-      particle.vx *= -1;
-    }
-    if (particle.y < 0 || particle.y > canvasHeight) {
-      particle.vy *= -1;
-    }
-  }); */
-  particles.forEach((particle) => {
-    quadTree.insert(particle);
-  });
-  quadTree.gravity();
+  // Update the particles positions
+  quadTree.treeForce(particles);
 
   // Erase canvas
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
   // Draw particles using the new state.
-  // !!!!Sync/jitter issues from async state updates? Seems fine, but monitor this.!!!!
   particles.forEach((particle) => {
     ctx.beginPath(); // Begin path for drawing the particle
     ctx.arc(particle.x, particle.y, 2, 0, 2 * Math.PI);
@@ -72,24 +38,16 @@ const animate = ({
   });
 
   // Draw the quadTree cells
-  const drawRects = (quadTree: QuadTree) => {
+  const drawRects = (quadTree: Quadtree) => {
     ctx.strokeRect(
-      quadTree.boundary.left,
-      quadTree.boundary.top,
+      quadTree.boundary.x,
+      quadTree.boundary.y,
       quadTree.boundary.width,
       quadTree.boundary.height
     );
-    if (quadTree.northwest) {
-      drawRects(quadTree.northwest);
-    }
-    if (quadTree.northeast) {
-      drawRects(quadTree.northeast);
-    }
-    if (quadTree.southeast) {
-      drawRects(quadTree.southeast);
-    }
-    if (quadTree.southwest) {
-      drawRects(quadTree.southwest);
+
+    for (const child of quadTree.children) {
+      drawRects(child);
     }
   };
 
