57bdcc31f3e0f0a77c86e52ff77f9b93662413a2
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-25 16:05:19 -0400

    Update test to use react testing library properly

--- src/__tests__/useParticles.test.ts
+++ src/__tests__/useParticles.test.ts
@@ -1,9 +1,13 @@
 import { test, expect } from "vitest";
 import useParticles from "../CanvasSim/useParticles";
+import { renderHook } from "@testing-library/react";
 
 test("Randomize properly randomizes all particle properties", () => {
   const testBoundary = { width: 100, height: 100 };
-  const testParticles = useParticles(1);
+
+  // Render the hook
+  const { result } = renderHook(() => useParticles(1));
+  const testParticles = result.current;
   testParticles.randomize(testBoundary.width, testBoundary.height);
 
   // Check for proper x and y values
