6b41675a5f2e8e805fb5d83e92c46c7f120256f1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-25 17:51:36 -0400

    Put color data into separate typed array

--- src/CanvasSim/useParticles.ts
+++ src/CanvasSim/useParticles.ts
@@ -1,20 +1,22 @@
 import { useRef } from "react";
+import { Particle } from "./defs";
 
 const useParticles = (particleCount: number) => {
-  // x, y, vx, vy, m, r are float32 and colorRGB is four int8 for a total of 28 bytes / particle
+  // x, y, vx, vy, m, r are float32 for a total of 24 bytes / particle
+  // colorRGB is four Uint8s for a total of 4 bytes / particle
   const xI = 0;
   const yI = 4;
   const vxI = 8;
   const vyI = 12;
   const mI = 16;
   const rI = 20;
-  const colorRI = 24;
-  const colorGI = 25;
-  const colorBI = 26;
-  const colorAI = 27;
 
   const particleBuffer = useRef<ArrayBuffer>(
-    new ArrayBuffer(particleCount * 28)
+    new ArrayBuffer(particleCount * 24)
+  );
+
+  const particleColorData = useRef<Uint8Array>(
+    new Uint8Array(particleCount * 4)
   );
 
   // Data view for interacting with buffer
@@ -35,23 +37,36 @@ const useParticles = (particleCount: number) => {
       const colorB = Math.random() * 255;
       const colorA = 255;
 
-      // Each particle has 28 bytes worth of properties
-      const index = i * 28;
+      // Set indexes for typed arrays
+      const dataIndex = i * 24;
+      const colorIndex = i * 4;
 
-      particleData.current.setFloat32(index + xI, x);
-      particleData.current.setFloat32(index + yI, y);
-      particleData.current.setFloat32(index + vxI, vx);
-      particleData.current.setFloat32(index + vyI, vy);
-      particleData.current.setFloat32(index + mI, m);
-      particleData.current.setFloat32(index + rI, r);
-      particleData.current.setUint8(index + colorRI, colorR);
-      particleData.current.setUint8(index + colorGI, colorB);
-      particleData.current.setUint8(index + colorBI, colorG);
-      particleData.current.setUint8(index + colorAI, colorA);
+      particleData.current.setFloat32(dataIndex + xI, x);
+      particleData.current.setFloat32(dataIndex + yI, y);
+      particleData.current.setFloat32(dataIndex + vxI, vx);
+      particleData.current.setFloat32(dataIndex + vyI, vy);
+      particleData.current.setFloat32(dataIndex + mI, m);
+      particleData.current.setFloat32(dataIndex + rI, r);
+
+      particleColorData.current[colorIndex] = colorR;
+      particleColorData.current[colorIndex + 1] = colorG;
+      particleColorData.current[colorIndex + 2] = colorB;
+      particleColorData.current[colorIndex + 3] = colorA;
     }
   };
 
-  const particles = { data: particleData.current, randomize };
+  // Method for pushing new particle or particles
+  const push = (particles: Particle | Particle[]) => {
+    // Create new buffer based on old one plus extended data
+    // Create new data view based on new buffer
+    // Update the refs
+  };
+
+  const particles = {
+    data: particleData.current,
+    colors: particleColorData.current,
+    randomize,
+  };
   return particles;
 };
 
