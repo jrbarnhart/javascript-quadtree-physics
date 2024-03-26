30860a43ea0ba0721344718baf52d424b5b3927f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-26 00:20:16 -0400

    Move particles on screen and reverse vx/y when oustide window

--- src/CanvasSim/animate.ts
+++ src/CanvasSim/animate.ts
@@ -24,25 +24,45 @@ const animate = ({
   // Erase canvas
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-  /*   // Draw particles using the new state.
-  particles.forEach((particle) => {
-    ctx.beginPath(); // Begin path for drawing the particle
-    ctx.arc(particle.x, particle.y, 2, 0, 2 * Math.PI);
-    ctx.fillStyle = particle.color; // Set fill style for the particle
-    ctx.fill(); // Fill the particle shape
-  }); */
-
+  // Draw the particles
   for (let i = 0; i < particles.data.length / particles.dataElements; i++) {
     const dataIndex = i * particles.dataElements;
     const colorIndex = i * particles.colorElements;
 
-    const x = particles.data[dataIndex];
-    const y = particles.data[dataIndex + 1];
+    let x = particles.data[dataIndex];
+    let y = particles.data[dataIndex + 1];
+    let vx = particles.data[dataIndex + 2];
+    let vy = particles.data[dataIndex + 3];
     const r = particles.data[dataIndex + 5];
     const rColor = particles.colors[colorIndex];
     const gColor = particles.colors[colorIndex + 1];
     const bColor = particles.colors[colorIndex + 2];
     const aColor = particles.colors[colorIndex + 3];
+
+    // Reverse x and y velocity when over boundary
+    if (x + r > canvasWidth) {
+      particles.data[dataIndex] = canvasWidth - r - 1;
+      particles.data[dataIndex + 2] = vx * -1;
+    } else if (x - r < 0) {
+      particles.data[dataIndex] = r + 1;
+      particles.data[dataIndex + 2] = vx * -1;
+    }
+    if (y + r > canvasHeight) {
+      particles.data[dataIndex + 1] = canvasHeight - r - 1;
+      particles.data[dataIndex + 3] = vy * -1;
+    } else if (y - r < 0) {
+      particles.data[dataIndex + 1] = r + 1;
+      particles.data[dataIndex + 3] = vy * -1;
+    }
+
+    x = particles.data[dataIndex];
+    vx = particles.data[dataIndex + 2];
+    y = particles.data[dataIndex + 1];
+    vy = particles.data[dataIndex + 3];
+
+    // Update x and y position based on velocity
+    particles.data[dataIndex] += vx;
+    particles.data[dataIndex + 1] += vy;
 
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2 * Math.PI);
